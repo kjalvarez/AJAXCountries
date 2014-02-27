8aa8dfd515a4fb73fc603c10dfa12cfb501f91e8
Karin <karin.j.alvarez@gmail.com>
2014-02-27 10:44:57 -0700

    DOM manipulation/ list of countries added

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,7 +1,16 @@
 $(function() {
 	$.ajax('/countries', {
 		success: function(data) {
-			console.log(data);
+			for (i=0; i<data.length; i++) {
+				console.log(data[i])
+				$('body').append('<br>')
+				$('body').append('<div>' + 'Name: ' + data[i].name + '<div>')
+				$('body').append('<div>' + 'French Name: ' + data[i].frenchName + '<div>')
+				$('body').append('<div>' + 'Local Name: ' + data[i].localName + '<div>')
+				$('body').append('<div>' + 'Region: ' + data[i].region + '<div>')
+
+			}
+
 		}
 	})
 })
@@ -10,6 +19,7 @@ $('#country-btn').click(function() {
 	$.ajax('/countries', {
 		success: function(data){
 			console.log('Data: ', data);
+
 		}
 	})
 })
